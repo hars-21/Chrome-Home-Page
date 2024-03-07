47e5a83531ff8284b6621d6e7c7af5760e68e918
Harshil Gupta <harshilgupta.2005@gmail.com>
2024-03-07 23:30:25 +0530

    search

---
+++ main.js
@@ -0,0 +1,8 @@
+// let search = document.getElementById("searchbar");
+
+// search.addEventListener("submit", () => {
+//   let query = search.value;
+//   let url = `https://www.google.com/search?q=${query}`;
+//   let res = fetch(url);
+//   console.log("hi");
+// });
